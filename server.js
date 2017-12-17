3aebb83b8c8a49215821a294fcd1f15d819f2c97
Artur Arsalanov <joomrise@gmail.com>
2017-12-17 21:27:50 +0300

    add endpoints

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json({type:'*/*'}));
 
 // device info
 const DEVICE_INFO = {
@@ -118,20 +121,30 @@ class VacuumCleaner {
       statistics: DEVICE_MOCK_DATA
     }
   }
+  getCurrentState() {
+    return {
+      isOn: this.state.isOn,
+      mode: this.state.mode,
+      power: this.state.power,
+      deviceInfo: this.state.deviceInfo
+    };
+  }
   getOnOffState() {
     return this.state.isOn;
   }
   turnOnOff() {
     this.state.isOn = !this.state.isOn;
+    return this.state.isOn;
   }
   getMode() {
     return this.state.mode;
   }
   setMode(newMode) {
-    if(newMode !== MODES.DRY || newMode !== MODES.WASHING) {
+    if(newMode !== MODES.DRY && newMode !== MODES.WASHING) {
       throw 'wrong mode argument';
     }
     this.state.mode = newMode;
+    return newMode;
   }
   charge() {
     this.state.power = 100;
@@ -167,12 +180,51 @@ const Singleton = (function () {
 
 const cleaner = Singleton.getInstance();
 
-const BASE_URL = `/api/${DEVICE_INFO.serialNumber}`;
+const BASE_URL = `/api/${DEVICE_INFO.serialNumber}/`;
 
-app.get(BASE_URL + '', (req, res) => {
-  var t = cleaner.state.isOn;
-  console.log(t)
+app.get(BASE_URL, (req, res) => {
+  res.json(cleaner.getCurrentState());
 });
+
+app.get(BASE_URL + 'onoff', (req, res) => {
+  res.json(cleaner.getOnOffState());
+});
+
+app.put(BASE_URL + 'onnoff', (req, res) => {
+  const isOn = cleaner.turnOnOff();
+  res.json({
+    isOn
+  });
+});
+
+app.get(BASE_URL + 'mode', (req, res) => {
+  res.json(cleaner.getMode());
+});
+
+app.put(BASE_URL + 'mode', (req, res) => {
+  const mode = cleaner.setMode(req.body.mode);
+  res.json({
+    mode
+  });
+});
+
+app.get(BASE_URL + 'power', (req, res) => {
+  res.json(cleaner.getPower());
+});
+
+app.put(BASE_URL + 'power', (req, res) => {
+  cleaner.charge();
+  res.json({
+    power: 100
+  });
+});
+
+app.get(BASE_URL + 'stats', (req, res) => {
+  res.json(cleaner.getStatistics());
+});
+
+
+
 
 if(process.env.NODE_ENV === 'production') {
   // production
