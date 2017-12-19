fb8b7323fb10fcfe1f94a9fcf2223ff6e4a2a4dc
Artur Arsalanov <joomrise@gmail.com>
2017-12-19 23:50:36 +0300

    style loader

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -7,23 +7,43 @@ import './Dashbaord.css';
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      gridIsReady: false
-    };
   }
   render() {
+    const {
+      isOn,
+      mode,
+      power,
+      deviceInfo,
+    } = this.props;
+
+    if(!deviceInfo) {
+      return <Loader />
+    }
     return (
-      <div>asd</div>
+      <div className="dashboard-block">
+
+      </div>
     );
   }
 }
 
 Dashboard.propTypes = {
-
+  isOn: PropTypes.bool,
+  mode: PropTypes.string,
+  power: PropTypes.number,
+  deviceInfo: PropTypes.shape({
+    manufacturer: PropTypes.string,
+    serialNumber: PropTypes.string,
+    date: PropTypes.string,
+    model: PropTypes.string
+  })
 };
 
 Dashboard.defaultProps = {
-
+  isOn: false,
+  mode: 'dry',
+  power: 0,
+  deviceInfo: null
 };
 
 export default Dashboard;
--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -4,12 +4,13 @@ import PropTypes from 'prop-types';
 import './Loader.css';
 
 const Loader = () => (
-  <div className="loader-wrapper">
-    <div className="loader">
-      <div className="loader-inner-circle" />
-      <div className="loader-inner-circle" />
-      <div className="loader-inner-circle" />
-      <div className="loader-inner-circle" />
+  <div className="loader">
+    <div className="obj1">
+      <div className="obj2">
+        <div className="obj3">
+          <div className="obj4"></div>
+        </div>
+      </div>
     </div>
   </div>
 );
